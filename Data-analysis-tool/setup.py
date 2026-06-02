df0009f5f68a4935abfb21df88b5817260b5d5a3
DilshanNimsara <e22024@eng.pdn.ac.lk>
2026-06-02 22:41:21 +0530

    Update setup.py

--- Data-analysis-tool/setup.py
+++ Data-analysis-tool/setup.py
@@ -3,13 +3,15 @@ from setuptools import setup, find_packages
 setup(
     name="data_analysis_tool",
     version="0.1.0",
+    description="Data Sanitization and Exploration Engine",
+    author="Dineth Perera",
     packages=find_packages(),
     install_requires=[
-        "pandas",
-        "numpy",
-        "plotly",
-        "scikit-learn",
-        "scipy",
-        "pydantic"
+        "pandas>=2.0.0",
+        "numpy>=1.24.0",
+        "plotly>=5.18.0",
+        "scipy>=1.11.0",
+        "statsmodels>=0.14.0"
     ],
+    python_requires=">=3.10",
 )
